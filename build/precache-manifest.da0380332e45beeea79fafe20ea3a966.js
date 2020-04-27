819cc91b1f3575f73fb9d0bb50c5d773f2d03706
Creative Tech Solutions <creative.joomdev@gmail.com>
2020-04-27 22:52:25 +0500

    Build Files

---
+++ build/precache-manifest.da0380332e45beeea79fafe20ea3a966.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76c2db631ee2674eaa395e2e7b821b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf3726165498b63fd2ed",
+    "url": "./static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "7fb2725a159cf1644f8a",
+    "url": "./static/css/main.18a32f0c.chunk.css"
+  },
+  {
+    "revision": "cf3726165498b63fd2ed",
+    "url": "./static/js/2.f4bb2a9d.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "./static/js/2.f4bb2a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb2725a159cf1644f8a",
+    "url": "./static/js/main.989bec94.chunk.js"
+  },
+  {
+    "revision": "eced64be0969d4d81f6b",
+    "url": "./static/js/runtime-main.67034472.js"
+  }
+]);
